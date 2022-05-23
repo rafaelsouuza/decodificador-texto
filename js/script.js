75fbaedb2db6f948b04949f20b992d782568d7e3
Rafael Souza <snrafa.souza@gmail.com>
2022-05-23 20:32:53 -0300

    Descriptografar

--- js/script.js
+++ js/script.js
@@ -1,59 +1,76 @@
-let buttonCript = document.getElementsByClassName("btn-cript")[0];
+var inputText;
 let click = true;
-buttonCript.addEventListener("click", criptText);
 
-function criptText() {
-    var text = document.getElementById("texto-cript").value.split('');
+let buttonCript = document.getElementsByClassName("btn-cript")[0];
+buttonCript.addEventListener("click", function criptText() {
+    inputText = document.getElementById("input-texto").value.split('');
 
-    for (let i = 0; i < text.length; i++) {
-
-        switch(text[i]) {
-            case "e": 
-                text[i] = "enter";
+    for (let i = 0; i < inputText.length; i++) {
+        switch (inputText[i]) {
+            case "e":
+                inputText[i] = "enter";
                 break;
-            case "i": 
-                text[i] = "imes";
+            case "i":
+                inputText[i] = "imes";
                 break;
-            case "a": 
-                text[i] = "ai";
+            case "a":
+                inputText[i] = "ai";
                 break;
-            case "o": 
-                text[i] = "ober";
+            case "o":
+                inputText[i] = "ober";
                 break;
-            case "u": 
-                text[i] = "ufat";
+            case "u":
+                inputText[i] = "ufat";
                 break;
         }
     }
 
-    let newTextCript = text.toString().replace(/,/g, "");
-
+    let newTextCript = inputText.toString().replace(/,/g, "");
     clearArticle();
-
     createTextAreaEl(newTextCript);
+});
 
-    document.getElementById("texto-cript").value = "";
-
-    console.log(newTextCript);
+let buttonDescript = document.getElementsByClassName("btn-descrip")[0];
+buttonDescript.addEventListener("click", function descripText() {
     
-}
+    for (let i = 0; i < inputText.length; i++) {
+        switch (inputText[i]) {
+            case "enter":
+                inputText[i] = "e";
+                break;
+            case "imes":
+                inputText[i] = "i";
+                break;
+            case "ai":
+                inputText[i] = "a";
+                break;
+            case "ober":
+                inputText[i] = "o";
+                break;
+            case "ufat":
+                inputText[i] = "u";
+                break;
+        }
+    }
+
+    let newTextDescrip = inputText.toString().replace(/,/g, "");
+    createTextAreaEl(newTextDescrip);
+});
 
 function clearArticle() {
     let node = document.getElementById("article");
     node.innerText = "";
 }
 
-    
-
 function createTextAreaEl(newTextCript) {
-
-    if(click) {
+    if (click) {
         let articleEl = document.getElementById("article");
 
         let divEl = document.createElement("div");
         divEl.setAttribute("class", "article-texto");
 
         let textareaEl = document.createElement("textarea");
+        textareaEl.setAttribute("id", "inputText-copy");
 
         let divButtonEl = document.createElement("div");
         divButtonEl.setAttribute("class", "article-btn");
@@ -65,16 +82,27 @@ function createTextAreaEl(newTextCript) {
         divEl.appendChild(textareaEl);
         divButtonEl.appendChild(buttonEl);
 
-        //divEl.classList.add(".article-texto");
-        //divButtonEl.classList.add(".aside-btn");
         buttonEl.setAttribute("type", "submit");
         buttonEl.setAttribute("class", "btn-copy");
         buttonEl.value = "Copiar";
 
         textareaEl.value = newTextCript;
-        
+
+        buttonEl.addEventListener("click", copyText);
+
         click = false;
+    } else {
+        document.getElementById("inputText-copy").value = newTextCript;
     }
-    
+}
+
+function copyText() {
+    let textCopy = document.getElementById("inputText-copy");
+    textCopy.select();
+    textCopy.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    console.log(textCopy.value);
+    textCopy.value = "";
+    document.getElementById("input-texto").value = "";
 }
 
